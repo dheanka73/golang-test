54cb012dc0d182176b205c9d1457dfd10174fd7d
Dhean Kasanda <dheanka73.io@gmail.com>
2025-04-16 19:47:51 +0700

    Try some silly stuff

--- hello-world.go
+++ hello-world.go
@@ -1,10 +1,12 @@
 package main
 
 import "fmt"
+import "time"
 
 func main() {
 
 	var name string
+	var number int
 
 	fmt.Print("What is your name ? : ")
 	fmt.Scan(&name)
@@ -13,6 +15,22 @@ func main() {
 		fmt.Println("Your lying !")
 	} else {
 		fmt.Println("Oh, Hi ", name, " !")
+
+		fmt.Print("Type any number, ", name, "!: ")
+		fmt.Scan(&number)
+
+		fmt.Println("Um......")
+		go func() {
+			time.Sleep(1 * time.Second)
+			fmt.Println("You know......")
+			time.Sleep(1 * time.Second)
+
+			fmt.Print()
+			for i := 0; i < number; i++ {
+				fmt.Println("I love you !")
+			}
+		}()
+		time.Sleep(time.Second * 3)
 	}
 
 }
